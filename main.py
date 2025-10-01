0eb60d37cdf4e3f45202b3e497beff4a40439cc7
open buidler <builderopen@gmail.com>
2025-10-02 00:26:35 +0530

    update: add threading

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import requests
 from bs4 import BeautifulSoup
 from openai import OpenAI
 from pydantic import BaseModel
+import concurrent.futures
 
 load_dotenv()
 #
@@ -29,44 +30,52 @@ class DigSchema(BaseModel):
     video_links: list[str]
     event_details: EventSchema
 
-start_digging=input("Enter the ETHGlobal project's showcase url: ")
+def dig_project(start_digging: str):
+    try:
+        response = requests.request("GET", start_digging)
+        soup = BeautifulSoup(response.text, "html.parser")
+        all_scripts = soup.find_all("script")
+        dig = None
+        for script in all_scripts:
+            if "fullUrl" in script.text:
+                response = client.responses.parse(
+                    model="gpt-4o-2024-08-06",
+                    input=[
+                        {
+                            "role": "system", "content": "Extract the information as per the given schema."
+                        },
+                        {
+                            "role": "user",
+                            "content": script.text,
+                        },
+                    ],
+                    text_format=DigSchema,
+                )
 
-try:
-    response = requests.request("GET", start_digging)
-    soup = BeautifulSoup(response.text, "html.parser")
-    all_scripts = soup.find_all("script")
-    dig = None
-    for script in all_scripts:
-        if "fullUrl" in script.text:
-            response = client.responses.parse(
-                model="gpt-4o-2024-08-06",
-                input=[
-                    {
-                        "role": "system", "content": "Extract the information as per the given schema."
-                    },
-                    {
-                        "role": "user",
-                        "content": script.text,
-                    },
-                ],
-                text_format=DigSchema,
+                dig = response.output_parsed
+
+        if dig is not None:
+            print(f"lookup: {dig.project_name}")
+            web_search = f'''
+            {dig.project_name} - {dig.project_description}, do a deep research using this links: {','.join(dig.github_links)}, and respond in only json format, with any lookalike projects ( Plagiarised checker), total number of commits, average lines edited per commit.
+            '''
+
+            response = client.responses.create(
+                model="gpt-5",
+                tools=[{"type": "web_search"}],
+                input=web_search
             )
 
-            dig = response.output_parsed
+            print(response.output_text)
 
-    if dig is not None:
-        print(f"lookup: {dig.project_name}")
-        web_search = f'''
-        {dig.project_name} - {dig.project_description}, do a deep research using this links: {','.join(dig.github_links)}, and respond in only json format, with any lookalike projects ( Plagiarised checker), total number of commits, average lines edited per commit.
-        '''
+    except Exception as error:
+        print(error)
 
-        response = client.responses.create(
-            model="gpt-5",
-            tools=[{"type": "web_search"}],
-            input=web_search
-        )
+projects = open("projects.txt", "r").read().splitlines()
+pool = concurrent.futures.ThreadPoolExecutor(max_workers=50)
 
-        print(response.output_text)
+for project in projects:
+    print(f"project: {project}")
+    pool.submit(dig_project, project)
 
-except Exception as error:
-    print(error)
+pool.shutdown(wait=True)
